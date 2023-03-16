d0db0e73d87785e62953bd40e0782fa23c9fd32c
shumphrey268 <shane.humphrey@protonmail.com>
2023-03-16 01:53:52 -0400

    Added functionality for alert messages (e.g. alert for successful account creation upon registration)

--- hockeysignup/hockeysignup/settings.py
+++ hockeysignup/hockeysignup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
+from django.contrib.messages import constants as messages
 import os
 
 # Quick-start development settings - unsuitable for production
@@ -73,6 +74,14 @@ INSTALLED_APPS = [
 
     'users.apps.UsersConfig'
 ]
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
